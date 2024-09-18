9468860cb3cf492bea879019a60f096c6968bdc7
Jonathan <jorgejonatthan@gmail.com>
2024-09-17 19:56:13 -0600

    mensaje personalizado

--- hello.sh
+++ hello.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "Hola mundo!"
+function main() {
+
+	declare -a args=("$@")
+	first_arg="${args[0]}"
+	
+	if [ -z "${first_arg}" ]; then 
+		echo "Hello, world!"
+	else 
+		echo "Hello, ${first_arg}"
+	fi
+}
+
+main "$@"
